79332460bc931285b8ac8872b19b1c16d5808921
UNeedCryDear <52729998+UNeedCryDear@users.noreply.github.com>
2022-03-29 15:44:49 +0800

    add yolov5-P6 model

--- yolo.h
+++ yolo.h
@@ -29,8 +29,10 @@ private:
 	const int strideSize = 4;  //stride size
 #else
 	const float netAnchors[3][6] = { { 10,13, 16,30, 33,23 },{ 30,61, 62,45, 59,119 },{ 116,90, 156,198, 373,326 } };
+	
 	const int netWidth = 640;   //ONNX图片输入宽度
 	const int netHeight = 640;  //ONNX图片输入高度
+	
 	const int strideSize = 3;   //stride size
 #endif // YOLO_P6
 
