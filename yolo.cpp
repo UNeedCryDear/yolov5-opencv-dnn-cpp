6f5d01970c6cff0a45c700525b37efcc3c875c61
UNeedCryDear <52729998+UNeedCryDear@users.noreply.github.com>
2022-03-29 15:22:54 +0800

    add yolov5-P6 model

--- yolo.cpp
+++ yolo.cpp
@@ -1,10 +1,9 @@
-#include"stdafx.h"
 #include"yolo.h"
 using namespace std;
 using namespace cv;
-using namespace dnn;
+using namespace cv::dnn;
 
-bool Yolo::readModel(Net &net, string &netPath,bool isCuda = false) {
+bool Yolo::readModel(Net& net, string& netPath, bool isCuda = false) {
 	try {
 		net = readNet(netPath);
 	}
@@ -15,30 +14,28 @@ bool Yolo::readModel(Net &net, string &netPath,bool isCuda = false) {
 	if (isCuda) {
 		net.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
 		net.setPreferableTarget(cv::dnn::DNN_TARGET_CUDA_FP16);
-
 	}
 	//cpu
 	else {
-		
 		net.setPreferableBackend(cv::dnn::DNN_BACKEND_DEFAULT);
 		net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
 	}
 	return true;
 }
-bool Yolo::Detect(Mat &SrcImg, Net &net, vector<Output> &output) {
+bool Yolo::Detect(Mat& SrcImg, Net& net, vector<Output>& output) {
 	Mat blob;
 	int col = SrcImg.cols;
 	int row = SrcImg.rows;
 	int maxLen = MAX(col, row);
 	Mat netInputImg = SrcImg.clone();
-	if (maxLen > 1.2*col || maxLen > 1.2*row) {
+	if (maxLen > 1.2 * col || maxLen > 1.2 * row) {
 		Mat resizeImg = Mat::zeros(maxLen, maxLen, CV_8UC3);
 		SrcImg.copyTo(resizeImg(Rect(0, 0, col, row)));
 		netInputImg = resizeImg;
 	}
-	blobFromImage(netInputImg, blob, 1 / 255.0, cv::Size(netWidth, netHeight), cv::Scalar(104, 117,123), true, false);
+	blobFromImage(netInputImg, blob, 1 / 255.0, cv::Size(netWidth, netHeight), cv::Scalar(0, 0, 0), true, false);
 	//如果在其他设置没有问题的情况下但是结果偏差很大，可以尝试下用下面两句语句
-	//blobFromImage(netInputImg, blob, 1 / 255.0, cv::Size(netWidth, netHeight), cv::Scalar(0, 0,0), true, false);
+	//blobFromImage(netInputImg, blob, 1 / 255.0, cv::Size(netWidth, netHeight), cv::Scalar(104, 117, 123), true, false);
 	//blobFromImage(netInputImg, blob, 1 / 255.0, cv::Size(netWidth, netHeight), cv::Scalar(114, 114,114), true, false);
 	net.setInput(blob);
 	std::vector<cv::Mat> netOutputImg;
@@ -52,32 +49,32 @@ bool Yolo::Detect(Mat &SrcImg, Net &net, vector<Output> &output) {
 	float ratio_w = (float)netInputImg.cols / netWidth;
 	int net_width = className.size() + 5;  //输出的网络宽度是类别数+5
 	float* pdata = (float*)netOutputImg[0].data;
-	for (int stride =0; stride < 3; stride++) {    //stride
+	for (int stride = 0; stride < strideSize; stride++) {    //stride
 		int grid_x = (int)(netWidth / netStride[stride]);
 		int grid_y = (int)(netHeight / netStride[stride]);
-		for (int anchor = 0; anchor < 3; anchor++) { //anchors
+		for (int anchor = 0; anchor < 3; anchor++) {	//anchors
 			const float anchor_w = netAnchors[stride][anchor * 2];
 			const float anchor_h = netAnchors[stride][anchor * 2 + 1];
 			for (int i = 0; i < grid_y; i++) {
 				for (int j = 0; j < grid_x; j++) {
-					float box_score = pdata[4]; //Sigmoid(pdata[4]);//获取每一行的box框中含有某个物体的概率
-					if (box_score > boxThreshold) {
+					float box_score = pdata[4]; ;//获取每一行的box框中含有某个物体的概率
+					if (box_score >= boxThreshold) {
 						cv::Mat scores(1, className.size(), CV_32FC1, pdata + 5);
 						Point classIdPoint;
 						double max_class_socre;
 						minMaxLoc(scores, 0, &max_class_socre, 0, &classIdPoint);
-						max_class_socre = (float)max_class_socre; //Sigmoid((float)max_class_socre);
-						if (max_class_socre > classThreshold) {
+						max_class_socre = (float)max_class_socre;
+						if (max_class_socre >= classThreshold) {
 							//rect [x,y,w,h]
-							float x = pdata[0];// (Sigmoid(pdata[0]) * 2.f - 0.5f + j) * netStride[stride];  //x
-							float y = pdata[1];// (Sigmoid(pdata[1]) * 2.f - 0.5f + i) * netStride[stride];   //y
-							float w = pdata[2];// powf(Sigmoid(pdata[2]) * 2.f, 2.f) * anchor_w;   //w
-							float h = pdata[3];// powf(Sigmoid(pdata[3]) * 2.f, 2.f) * anchor_h;  //h
-							int left = (x - 0.5*w)*ratio_w;
-							int top = (y - 0.5*h)*ratio_h;
+							float x = pdata[0];  //x
+							float y = pdata[1];  //y
+							float w = pdata[2];  //w
+							float h = pdata[3];  //h
+							int left = (x - 0.5 * w) * ratio_w;
+							int top = (y - 0.5 * h) * ratio_h;
 							classIds.push_back(classIdPoint.x);
-							confidences.push_back(max_class_socre*box_score);
-							boxes.push_back(Rect(left, top, int(w*ratio_w), int(h*ratio_h)));
+							confidences.push_back(max_class_socre * box_score);
+							boxes.push_back(Rect(left, top, int(w * ratio_w), int(h * ratio_h)));
 						}
 					}
 					pdata += net_width;//下一行
@@ -88,7 +85,7 @@ bool Yolo::Detect(Mat &SrcImg, Net &net, vector<Output> &output) {
 
 	//执行非最大抑制以消除具有较低置信度的冗余重叠框（NMS）
 	vector<int> nms_result;
-	NMSBoxes(boxes, confidences, classThreshold*boxThreshold, nmsThreshold, nms_result);
+	NMSBoxes(boxes, confidences, nmsScoreThreshold, nmsThreshold, nms_result);
 	for (int i = 0; i < nms_result.size(); i++) {
 		int idx = nms_result[i];
 		Output result;
@@ -97,14 +94,13 @@ bool Yolo::Detect(Mat &SrcImg, Net &net, vector<Output> &output) {
 		result.box = boxes[idx];
 		output.push_back(result);
 	}
-
 	if (output.size())
 		return true;
 	else
 		return false;
 }
 
-void Yolo::drawPred(Mat &img, vector<Output> result, vector<Scalar> color) {
+void Yolo::drawPred(Mat& img, vector<Output> result, vector<Scalar> color) {
 	for (int i = 0; i < result.size(); i++) {
 		int left, top;
 		left = result[i].box.x;
@@ -112,8 +108,8 @@ void Yolo::drawPred(Mat &img, vector<Output> result, vector<Scalar> color) {
 		int color_num = i;
 		rectangle(img, result[i].box, color[result[i].id], 2, 8);
 
-		string label = className[result[i].id] +":" + to_string(result[i].confidence);
-							 
+		string label = className[result[i].id] + ":" + to_string(result[i].confidence);
+
 		int baseLine;
 		Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
 		top = max(top, labelSize.height);
